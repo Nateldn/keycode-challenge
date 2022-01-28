50b9fe4f778fdc980ea26186418e0a75498292df
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-01-28 09:14:06 +0000

    Refactor JS, Add main tag

--- app.js
+++ app.js
@@ -1,58 +1,51 @@
-
-
-let myEventCode = document.getElementById("event_code"); // select element with the id event_code
-
-
-let myEventKey = document.getElementById("event_key"); // select element with the id event_key
-
+let hideIntroText = document.getElementById("intro_text"); // select intro_text H1
+// let keyPressResults = document.getElementById("keypress_results").style.display = "none"; // intially hide keypress results
+let myBigNum = document.getElementById("big_num"); // select key h1 tag [Used to display key code in large font ]
+let myEventKey = document.getElementById("event_key"); // select element with the id event_code
 let myEventWhich = document.getElementById("event_which"); // select element with the id event_which
+let myEventCode = document.getElementById("event_code"); // select element with the id event_key
 
-let myBigNum = document.getElementById("big_num");
-
-console.log(myEventCode);
-console.log(myEventKey);
-console.log(myEventWhich);
-
-
-
-
-
-
-
-let keyResults = document.getElementById("keypress_results")
-const revealKeypressResults  = () => {
-  if (keyResults.style.display === "none") {
-    keyResults.style.display = "block";
-  } else {
-    keyResults.style.display = "none";
-  }
-}
-
-
-
+//console.log(keyPressResults);
+// console.log(myEventCode);
+// console.log(myEventKey);
+// console.log(myEventWhich);
 
 document.addEventListener("keydown", (e) => {
   console.log(e)
 
-myEventCode.textContent = e.key; // display the keydown event {key} within the empty paragraph
-myEventKey.textContent = e.keyCode; // display the keydown event {keyCode} within the empty paragraph
+myBigNum.textContent = e.which;
+myEventKey.textContent = e.key; // display the keydown event {key} within the empty paragraph
 myEventWhich.textContent = e.which; // display the keydown {eventWhich} within the empty paragraph
-myBigNum.textContent = e.keyCode
+myEventCode.textContent = e.code; // display the keydown event {keyCode} within the empty paragraph
+hideIntroText.style.display = "none"; // Hide intro text once a key is press
+keyPressResults.style.display ="";
+
 
 });
 
 
 
-
-
-// p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
-//https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event#addeventlistener_keydown_example
-// const log = document.getElementById('event_code');
-
-// document.addEventListener('keydown', logKey);
-
-// function logKey(e) {
-//   log.textContent += ` ${e.code}`;
+// const revealKeyPressResults = () => {
+//   if (keyPressResults == "none") {
+//     revealKeyPressResult.display = "block";
+//   }
+//   else {}
 // }
+// console.log(revealKeyPressResults());
+
+
+
+// let keyResults = document.getElementById("keypress_results")
+// const revealKeypressResults  = () => {
+//   if (keyResults.style.display === "none") {
+//     keyResults.style.display = "block";
+//   } else {
+//     keyResults.style.display = "none";
+//   }
+// }
+
+
+
+
 
 
